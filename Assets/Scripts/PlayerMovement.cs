658b47f519fca50b9bdc6d019555a2b7b770063a
cekomo <cemil.sahin@ozu.edu.tr>
2022-12-05 21:14:44 +0300

    add animation facing right/left

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,50 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float playerSpeed = 7.5f;
+    private float previousMove;
 
     private Vector2 playerMovementVector2;
     private Rigidbody2D rb2Player;
 
-    void Start()
+    public Animator animator;  
+
+    private void Start()
     {
         rb2Player = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+        // they get values of 0 and 1 in case the button is pressed or not
         playerMovementVector2.x = Input.GetAxisRaw("Horizontal");
-        playerMovementVector2.y = Input.GetAxisRaw("Vertical");
-
-        MoveCharacter(rb2Player, playerMovementVector2, playerSpeed);
+        // playerMovementVector2.y = Input.GetAxisRaw("Vertical");
+        
+        FaceTowards();
+        MoveCharacter(playerMovementVector2, playerSpeed);
     }
     
-    public void MoveCharacter(Rigidbody2D rb2Player, Vector2 playerMovementVector2, float playerSpeed)
+    private void MoveCharacter(Vector2 playerMovementVector2, float playerSpeed)
     {
         // move the rigidBody2D instead of moving the transform to prevent camera shaking 
         //..during wall contact
         playerMovementVector2.Normalize();
         rb2Player.velocity = playerMovementVector2 * playerSpeed;
+
+        AnimateRunning(rb2Player.velocity.x);
+    }
+
+    private void FaceTowards()
+    {
+        if (previousMove == playerMovementVector2.x) return;
+        
+        if (playerMovementVector2.x != 0)
+            transform.localScale = new Vector3(5*playerMovementVector2.x, 5, 1);
+ 
+        previousMove = playerMovementVector2.x;
+    }
+    
+    private void AnimateRunning(float playerVelocity)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(playerVelocity));
     }
 }
