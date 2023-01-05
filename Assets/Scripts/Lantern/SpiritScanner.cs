2208aa38ee43cb7b1f55c034873a80de9c534205
cekomo <cemil.sahin@ozu.edu.tr>
2023-01-05 13:53:42 +0300

    try to fix spirit activation (buggy)

--- Assets/Scripts/Lantern/SpiritScanner.cs
+++ Assets/Scripts/Lantern/SpiritScanner.cs
@@ -12,6 +12,7 @@ namespace Lantern
         // private List<CircleCollider2D> spiritColliders;
 
         private Collider2D[] spiritColliders;
+        private SpriteRenderer[] spiritSprites;
         
         private float scanCooldown;
         private float scannerRadius; 
@@ -20,36 +21,50 @@ namespace Lantern
         {
             scannerRadius = GetComponent<CircleCollider2D>().radius;
             spiritColliders = new Collider2D[spiritController.spirits.Count];
+            spiritSprites = new SpriteRenderer[spiritController.spirits.Count];
 
+            for (var i = 0; i < spiritController.spirits.Count; i++)
+                spiritSprites[i] = spiritController.spirits[i].GetComponent<SpriteRenderer>();
             for (var i = 0; i < spiritController.spirits.Count; i++)
                 spiritColliders[i] = spiritController.spirits[i].GetComponent<Collider2D>();
         }
 
         private void Update()
         {
+            // print(scanCooldown);
             if (scanCooldown > 0)
             {
                 scanCooldown -= Time.deltaTime;
                 return;
             }
 
-            print(scanCooldown);
+            
             ScanSpirit();
         }
         
         private void ScanSpirit()
         {
             if (!Input.GetKey(KeyCode.Z)) return;
-
-            scanCooldown = 10f;
-            for (var i = 0; i < spiritColliders.Length; i++)
-                if (Physics2D.OverlapCircleNonAlloc(transform.position, scannerRadius, spiritColliders, i) > 0)
-                {
-                    spiritController.spirits[i].SetActive(true);
-                }
             
+            scanCooldown = 2f;
+            var numColliders = Physics2D.OverlapCircleNonAlloc(transform.position, scannerRadius, spiritColliders);
+            
+            for (var i = 0; i < numColliders; i++)
+            {
+                var thecollider = spiritColliders[i];
+                var thespirit = thecollider.gameObject;
+                var spriteRenderer = thespirit.GetComponent<SpriteRenderer>();
+                spriteRenderer.enabled = true;
+
+                // spiritSprites[i].enabled = false;
+            }
         }
         
     }
 
 }
+// for (var i = 0; i < spiritColliders.Length; i++)
+// if (Physics2D.OverlapCircleNonAlloc(transform.position, scannerRadius, spiritColliders) == i)
+// {
+//     spiritController.spirits[i].SetActive(true);
+// }
