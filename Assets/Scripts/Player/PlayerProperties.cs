ac37858ac41192ebe8b6f699fb85a322ca49984c
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-01-07 13:12:05 +0300

    refactor: add player attribute super-class

---
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+    private Rigidbody2D rbPlayer;
+    
+    public Animator PlayerAnimator;
+    public LayerMask PlatformsLayerMask;
+    
+    [HideInInspector] public CapsuleCollider2D CapsuleCollider;
+
+    public void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody2D>();
+        CapsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+}
