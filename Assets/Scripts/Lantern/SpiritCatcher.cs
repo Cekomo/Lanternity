f913973a711b5850b6e94ccafa61f21be8dff01d
cekomo <cemil.sahin@ozu.edu.tr>
2023-01-19 00:28:17 +0300

    insert textmeshpro to show spirits captured

--- Assets/Scripts/Lantern/SpiritCatcher.cs
+++ Assets/Scripts/Lantern/SpiritCatcher.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using Light;
 using Player;
+using UserInterface;
 
 namespace Lantern
 {
     public class SpiritCatcher : MonoBehaviour
     {
+        public SpiritCounterUI spiritCounterUI;
+        
+        private int spiritCaptured;
+        
         private void OnTriggerStay2D(Collider2D col)
         {
             if (!col.gameObject.CompareTag("Spirit") || 
@@ -14,7 +19,10 @@ namespace Lantern
             var spriteRenderer = col.gameObject.GetComponent<SpriteRenderer>();
             if (spriteRenderer.enabled)
                 col.gameObject.SetActive(false);
-            // not finished...
+
+            spiritCaptured++;
+            spiritCounterUI.SetSpiritCaptured(spiritCaptured);
+            
             LightIntensityController.LanternState = LanternFlickState.CatchSpirit;
         }
     }
--- Assets/Scripts/UserInterface/SpiritCounterUI.cs
+++ Assets/Scripts/UserInterface/SpiritCounterUI.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UserInterface
 {
     public class SpiritCounterUI : MonoBehaviour
     {
-
+        [SerializeField] private TextMeshProUGUI  textMeshProUGUI;
+        
+        private void Start()
+        {
+            textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        }
+        
+        public void SetSpiritCaptured(int spiritAmount)
+        {
+            textMeshProUGUI.text = "<sprite name=\"S" + spiritAmount + "\">";
+        }
     }
 
 }
